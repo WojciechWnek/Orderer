3bcf148b20804c5dbd1527dfcc88f696c70a8598
WojciechWnek <wownek@gmail.com>
2025-02-24 14:21:31 +0100

    Setup and Models

--- drf_course/settings.py
+++ drf_course/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from django.conf.global_settings import AUTH_USER_MODEL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -111,3 +113,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'api.User'
