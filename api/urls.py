6496688a5eeb973beb701f9e1ba41b2de607420e
WojciechWnek <wownek@gmail.com>
2025-02-25 09:27:19 +0100

    Serializers & Response objects

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('json-products/', views.json_product_list),
+    path('json-products/<int:pk>/', views.json_product_detail),
+    path('products/', views.product_list),
+    path('products/<int:pk>/', views.product_detail)
 ]
--- api/views.py
+++ api/views.py
@@ -1,1 +1,35 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from api.serializers import ProductSerializer
+from api.models import Product
+
+# Django way
+def json_product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return JsonResponse({
+        "data": serializer.data
+    })
+
+def json_product_detail(request, pk):
+    product = get_object_or_404(Product, pk = pk)
+    serializer = ProductSerializer(product)
+    return JsonResponse({
+        "data": serializer.data
+    })
+
+# Rest Framework way
+@api_view(["GET"])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk = pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
