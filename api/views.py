2fa03c2ded992d82c59f0075f2d9d23ed13474df
WojciechWnek <wownek@gmail.com>
2025-03-04 11:31:37 +0100

    12: JWT Authentication with djangorestframework-simplejwt

--- api/views.py
+++ api/views.py
@@ -1,5 +1,4 @@
 from django.db.models import Max
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework.permissions import (
